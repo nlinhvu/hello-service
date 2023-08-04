fcfa8586d0ca573fc7ddcffc5646542b804587b2
nlinhvu <nlinhvu.dev@gmail.com>
2023-08-05 00:44:36 +0700

    first commit

---
+++ src/main/java/vn/cloud/helloservice/HelloServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.cloud.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/cloud/helloservice/HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.cloud.helloservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
