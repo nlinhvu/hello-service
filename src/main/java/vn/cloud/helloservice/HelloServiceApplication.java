a12e189ef44376edb8ac52d6f7b024ec517967fa
nlinhvu <nlinhvu.dev@gmail.com>
2023-08-13 22:03:04 +0700

    video3: add jeager and zipkin

--- src/main/java/vn/cloud/helloservice/HelloServiceApplication.java
+++ src/main/java/vn/cloud/helloservice/HelloServiceApplication.java
@@ -2,6 +2,12 @@ package vn.cloud.helloservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HelloServiceApplication {
@@ -10,4 +16,24 @@ public class HelloServiceApplication {
 		SpringApplication.run(HelloServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@RestController
+	class HelloController {
+
+		private final RestTemplate restTemplate;
+
+		HelloController(RestTemplate restTemplate) {
+			this.restTemplate = restTemplate;
+		}
+
+		@GetMapping("/hello")
+		public String hello() {
+			ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("https://httpbin.org/post", "Hello, Cloud!", String.class);
+			return responseEntity.getBody();
+		}
+	}
 }
