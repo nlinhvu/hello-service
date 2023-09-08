c5b8b8fb60a9d009da9e0d908dabe80340b7c16d
nlinhvu <nlinhvu.dev@gmail.com>
2023-09-08 11:04:29 +0700

    video6: add common tags

---
+++ src/main/java/vn/cloud/helloservice/MetricsFilterConfig.java
@@ -0,0 +1,24 @@
+package vn.cloud.helloservice;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.config.MeterFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.ZoneId;
+import java.util.List;
+import java.util.UUID;
+
+@Configuration
+public class MetricsFilterConfig {
+
+    @Bean
+    public MeterFilter commonTagsMeterFilter() {
+        return MeterFilter.commonTags(
+                List.of(
+                        Tag.of("instance.uuid", UUID.randomUUID().toString()),
+                        Tag.of("zone.id", ZoneId.of("Europe/Berlin").toString())
+                )
+        );
+    }
+}
